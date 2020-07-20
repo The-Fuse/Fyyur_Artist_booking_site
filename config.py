93e77c124e5b3b62f6cfe3727f64b080dc0a1efa
Rohit kumar <rohitkumaryadav141002@gmail.com>
2020-07-20 17:28:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,5 +10,5 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgres://postgres:rohit@100@localhost:5432/fyyur'
+SQLALCHEMY_DATABASE_URI = 'postgres://postgres@localhost:5432/fyyur'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
